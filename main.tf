001fb96fd7389c653808411cfa70b48e012de1e3
imjoseangel <josea.munoz@gmail.com>
2022-03-17 08:02:45 +0100

    fix block

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_acl" "main" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" {
+resource "aws_s3_bucket_versioning" "main" {
   bucket = aws_s3_bucket.main.id
   versioning_configuration {
     status = "Enabled"
