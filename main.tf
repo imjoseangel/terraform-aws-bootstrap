d8454f4d765711616c2020cbf0fa5b9354cedbcd
imjoseangel <josea.munoz@gmail.com>
2022-03-17 11:32:00 +0100

    add s3 and dynamo

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 #-------------------------------
 # S3 Bucket
 #-------------------------------
+#tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "main" {
   bucket        = var.name
   force_destroy = false
@@ -29,6 +30,7 @@ resource "aws_s3_bucket_versioning" "main" {
 resource "aws_kms_key" "main" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
+  enable_key_rotation     = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
@@ -42,15 +44,30 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  ignore_public_acls      = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  depends_on              = [aws_s3_bucket.main]
+}
+
 #-------------------------------
 # DynamoDB
 #-------------------------------
-
+#tfsec:ignore:aws-dynamodb-enable-recovery
 resource "aws_dynamodb_table" "main" {
-  name         = var.dynamodb
-  billing_mode = "PAY_PER_REQUEST"
+  name           = var.dynamodb
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  read_capacity  = 2
+  write_capacity = 2
 
-  hash_key = "LockID"
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.main.arn
+  }
 
   attribute {
     name = "LockID"
