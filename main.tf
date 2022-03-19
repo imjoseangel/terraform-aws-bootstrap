35b55f885faf4396ab5c7223739932b3ebd31023
imjoseangel <josea.munoz@gmail.com>
2022-03-19 11:43:42 +0100

    fix fmt

--- main.tf
+++ main.tf
@@ -58,9 +58,9 @@ resource "aws_s3_bucket_public_access_block" "main" {
 #-------------------------------
 #tfsec:ignore:aws-dynamodb-enable-recovery
 resource "aws_dynamodb_table" "main" {
-  name           = var.dynamodb
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = var.dynamodb
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
   #TODO: read_capacity  = 2
   #TODO: write_capacity = 2
 
